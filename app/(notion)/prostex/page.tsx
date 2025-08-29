ed8cae10256daec89d96e0f0137b09f7a3c36bcc
Joey <55892851+JioeyKun@users.noreply.github.com>
2025-08-29 09:46:12 +0800

    misc: updates prostex iframe link

--- app/(notion)/prostex/page.tsx
+++ app/(notion)/prostex/page.tsx
@@ -4,7 +4,7 @@ export default function Prostex() {
   return (
     <div style={{height: "calc(100vh + 40px)", marginTop: "-40px", pointerEvents: "none"}}>
       <EmbedFrame
-        src="https://joey-zhu.notion.site/ebd/25daea42556880aaa8efc920f642b01b?v=25daea42556880049b21000cbf6ce1d7"
+        src="https://joey-zhu.notion.site/ebd/25eaea425568805cb23cce7c7cfa34da"
         // height={"95vh"}
         // title="My Video"
       />
